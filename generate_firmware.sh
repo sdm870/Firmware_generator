db9367516e3608dfcd413290dd7f604f33bd1dad
althafvly <althafvly@gmail.com>
2023-06-05 19:56:35 +0530

    improve script a bit

--- generate_firmware.sh
+++ generate_firmware.sh
@@ -2,13 +2,41 @@
 
 set -e
 
-arch=$(arch)
-if [ $arch == x86_64 ]; then
-    arch=amd64
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Function to install aria2c
+install_aria2c() {
+    if command_exists apt-get; then
+        sudo apt-get update
+        sudo apt-get install -y aria2
+    elif command_exists dnf; then
+        sudo dnf install -y aria2
+    elif command_exists yum; then
+        sudo yum install -y aria2
+    elif command_exists pacman; then
+        sudo pacman -Sy --noconfirm aria2
+    elif command_exists zypper; then
+        sudo zypper install -y aria2
+    else
+        echo "Unable to install aria2c. Please install it manually."
+        exit 1
+    fi
+}
+
+# Check if aria2c is installed, if not, install it
+if ! command_exists aria2c; then
+    echo "aria2c is not installed. Installing..."
+    install_aria2c
 fi
 
-if [ $arch == aarch64 ]; then
-    arch=arm64
+arch=$(arch)
+if [ "$arch" == "x86_64" ]; then
+    arch="amd64"
+elif [ "$arch" == "aarch64" ]; then
+    arch="arm64"
 fi
 
 payload_link="https://github.com/ssut/payload-dumper-go/releases/download/1.2.2/payload-dumper-go_1.2.2_linux_$arch.tar.gz"
@@ -22,17 +50,20 @@ zips="https://bigota.d.miui.com/V14.0.3.0.TKHINXM/miui_ALIOTHINGlobal_V14.0.3.0.
       https://bigota.d.miui.com/V14.0.6.0.TKHCNXM/miui_ALIOTH_V14.0.6.0.TKHCNXM_7b7bb4a7bc_13.0.zip"
 
 echo "Downloading payload dumper..."
-wget --quiet $payload_link -O payload-dumper-go.tar.gz
-tar -xf payload-dumper-go.tar.gz payload-dumper-go && rm payload-dumper-go.tar.gz
+aria2c --file-allocation=none -x 16 "$payload_link"
+tar -xf payload-dumper-go_*.tar.gz payload-dumper-go && rm payload-dumper-go_*.tar.gz
 
 partitions="abl,aop,bluetooth,cmnlib,cmnlib64,devcfg,dsp,featenabler,hyp,imagefv,keymaster,modem,qupfw,tz,uefisecapp,xbl,xbl_config"
 
 for i in $zips
 do
-    echo "Downloading $(basename $i).."
-    wget --quiet -O $(basename $i) $i
-    ./payload-dumper-go -p $partitions -o . $(basename $i) >/dev/null 2>&1
-    echo "Zipping firmware_$(basename $i)..."
-    zip -r -q firmware_$(basename $i) META-INF *.img flash_firmware* && rm *.img
-    rm $(basename $i)
+    echo "Downloading $(basename "$i").."
+    aria2c --file-allocation=none -x 16 "$i"
+    echo "Extracting firmware from $(basename "$i")..."
+    ./payload-dumper-go -p "$partitions" -o . "$(basename "$i")" >/dev/null 2>&1
+    echo "Zipping firmware_$(basename "$i")..."
+    zip -r -q firmware_"$(basename "$i")" META-INF *.img flash_firmware* && rm *.img
+    rm "$(basename "$i")"
 done
+
+echo "Firmware extraction completed."
